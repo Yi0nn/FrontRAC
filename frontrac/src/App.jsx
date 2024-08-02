25cd317a7e00f62b81026a7935b61fb0fddc1df7
Yion <Stefhania.noguera@correounivalle.edu.co>
2024-08-02 01:05:34 -0500

    Changes

--- frontrac/src/App.jsx
+++ frontrac/src/App.jsx
@@ -3,17 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DynamicForm from './components/DynamicForm';
+import logo from './logo.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <header><h1 className='creative-title'>INFORMACION PARA RAC</h1></header>
-        <div>
-          <DynamicForm />
-        </div>
+      <header className="header-container">
+        {/* Agrega la imagen */}
+        <img src={logo} alt="Descripción de la imagen" />
+        <h1 className='creative-title'>INFORMACION PARA RAC</h1>
+        
+      </header>
+      <div>
+        <DynamicForm />
+      </div>
     </>
   )
 }
+
 export default App
